d67695907a61c4bf0fbec2d3620c7891f3e4c0ac
Jennifer Chen <jenn.chenn93@gmail.com>
2022-07-22 23:23:08 -0400

    chore: Use prettierrc config file

--- eslintrc.js
+++
@@ -1,6 +0,0 @@
-// The example settings below tell ESLint to error when single-quotes are used, and when semi-colons are missing.
-
-export const rules = {
-  quotes: ["error", "double"],
-  semi: ["error", "always"],
-};
