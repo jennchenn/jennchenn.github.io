9779c4a326ad1f4ea23b979bb001300b9d2f57c9
Jennifer Chen <jenn.chenn93@gmail.com>
2024-09-29 23:27:11 -0400

    enhancement: Adjust cactus drawing height

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,38 +1,65 @@
-import React, {useState} from "react";
-import cactus from "../assets/cactus.svg"
-import flower from "../assets/flower.svg"
+import React, { useState } from "react";
+import cactus from "../assets/cactus.svg";
+import flower from "../assets/flower.svg";
+
 const footer = {
   marginBottom: "40px",
   paddingLeft: "40px",
   paddingRight: "40px",
   textAlign: "center",
 };
+
 const imageDiv = {
   marginBottom: "40px",
-  height: "291px",
+  height: "300px",
   minWidth: "146px",
   overflow: "clip",
   display: "flex",
-}
+  flexDirection: "column",
+  justifyContent: "flex-end",
+};
 
-const image = {
+const imageCactus = {
   width: "100%",
-  height: "100%",
+  height: "226px",
   marginLeft: "auto",
   marginRight: "auto",
-}
+  cursor: "pointer",
+};
 
+const imageFlower = {
+  width: "100%",
+  height: "300px",
+  marginLeft: "auto",
+  marginRight: "auto",
+  cursor: "pointer",
+};
 
 function Footer() {
-  const [imagePath, setImagePath] = useState(cactus)
+  const [imagePath, setImagePath] = useState(cactus);
 
   return (
     <div style={footer}>
-      <div style={imageDiv} >
-      <img style={image} src={imagePath} alt="cactus drawing" onMouseEnter={() => setImagePath(flower)} onMouseLeave={() => setImagePath(cactus)}>
-      </img>
+      <div style={imageDiv}>
+        <img
+          style={imagePath === cactus ? imageCactus : imageFlower}
+          src={imagePath}
+          alt="cactus drawing"
+          onMouseEnter={() => setImagePath(flower)}
+          onMouseLeave={() => setImagePath(cactus)}
+        ></img>
       </div>
-      <div >made with <span role="img" aria-label="rice bowl emoji">🍚</span> and <span role="img" aria-label="yellow heart emoji">💛</span> © jenn c 2024</div>
+      <div>
+        made with{" "}
+        <span role="img" aria-label="rice bowl emoji">
+          🍚
+        </span>{" "}
+        and{" "}
+        <span role="img" aria-label="yellow heart emoji">
+          💛
+        </span>{" "}
+        © jenn c 2024
+      </div>
     </div>
   );
 }
